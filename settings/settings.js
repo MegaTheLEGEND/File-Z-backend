c2396246a37258118c6bbdbb3a4ce1f005ca73be
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-05-20 17:34:55 -0700

    Update settings.js

--- settings/settings.js
+++ settings/settings.js
@@ -1,46 +1,50 @@
-fetch('https://raw.githack.com/MegaTheLEGEND/File-Z-backend/main/tools/list.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Unable to retrieve data');
+ // Function to set the document icon
+  function setDocumentIcon(iconUrl) {
+    const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'icon';
+    link.href = iconUrl;
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
+
+  // Function to save tab settings to local storage and update document title and icon
+  function saveTabSettings() {
+    const tabName = document.getElementById('tab-name').value;
+    const tabIcon = document.getElementById('tab-icon').value;
+
+    const tabSettings = {
+      name: tabName,
+      icon: tabIcon
+    };
+
+    localStorage.setItem('tabSettings', JSON.stringify(tabSettings));
+    document.title = tabName; // Set the document title
+    setDocumentIcon(tabIcon); // Set the document icon
+  }
+
+  // Function to reset tab settings to default
+  function resetTabSettings() {
+    document.getElementById('tab-name').value = '';
+    document.getElementById('tab-icon').value = '';
+    document.title = 'SETTINGS'; // Reset the document title
+    setDocumentIcon('images/Z.jpg'); // Reset the document icon
+  }
+
+  // Event listener for Save button
+  document.getElementById('save-btn').addEventListener('click', saveTabSettings);
+
+  // Event listener for Reset button
+  document.getElementById('reset-btn').addEventListener('click', resetTabSettings);
+
+  // Load tab settings when the page is loaded
+  window.addEventListener('DOMContentLoaded', loadTabSettings);
+
+  function loadTabSettings() {
+    const tabSettings = JSON.parse(localStorage.getItem('tabSettings'));
+    if (tabSettings) {
+      document.getElementById('tab-name').value = tabSettings.name;
+      document.getElementById('tab-icon').value = tabSettings.icon;
+      document.title = tabSettings.name; // Set the document title
+      setDocumentIcon(tabSettings.icon); // Set the document icon
     }
-    return response.json();
-  })
-  .then(data => {
-    const linkList = document.getElementById('linkList');
-    const listTitle = document.querySelector('.list-title');
-    listTitle.textContent = data.listTitle;
-
-    const searchInput = document.querySelector('.search-input');
-    searchInput.addEventListener('input', searchLinks);
-
-    const linkItems = data.urls.map(url => {
-      const listItem = document.createElement('li');
-      const anchor = document.createElement('a');
-      anchor.href = url;
-      anchor.textContent = url;
-
-      listItem.appendChild(anchor);
-      return listItem;
-    });
-
-    linkItems.forEach(item => {
-      linkList.appendChild(item);
-    });
-
-    function searchLinks() {
-      const query = searchInput.value.toLowerCase();
-      linkItems.forEach(item => {
-        const linkText = item.querySelector('a').textContent.toLowerCase();
-        if (linkText.includes(query)) {
-          item.style.display = 'block';
-        } else {
-          item.style.display = 'none';
-        }
-      });
-    }
-  })
-  .catch(error => {
-    console.log('Error fetching link list:', error);
-    const linkList = document.getElementById('linkList');
-    linkList.textContent = 'Are you offline? We could not retrieve the data.';
-  });
+  }
