6398d85824f7d0062521b6d6f50375ab00b80a7c
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-05-21 13:24:27 -0700

    Create boot.js

---
+++ boot.js
@@ -0,0 +1,25 @@
+   
+var online = "true"
+
+let lines = [
+      "Welcome to the Terminal!",
+      "",
+      "Running command: boot File-Z",
+      "Booting...",
+      "Booting completed.",
+      "",
+      "checking for network...",
+      "initializing connection... <##########################> 100%",
+      "Network Connected!",
+      "",
+      "Running command: start Application-Z",
+      "Starting Application-Z...",
+      "Application-Z started successfully.",
+      "",
+      "Running command: runScript script-Z",
+      "Executing script-Z...",
+      "Script-Z execution completed.",
+      "",
+      "Running command: exit",
+      "Exiting Terminal..."
+    ];
