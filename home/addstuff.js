eb6226db8affc4e0c4bf93167fabdaf8f7027bb0
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-05-22 14:12:57 -0700

    Update addstuff.js

--- home/addstuff.js
+++ home/addstuff.js
@@ -1,1 +1,16 @@
+// find the element
+var element = document.getElementById('stuff');
+element.innerHTML = '<h2> share with your friends! Share this download link.</h2> <br> <h4><a href="https://drive.google.com/u/0/uc?id=1-ppKHWgN6OuJC4MBJBRAdTI6cLgRQUQO&export=download" >https://drive.google.com/u/0/uc?id=1-ppKHWgN6OuJC4MBJBRAdTI6cLgRQUQO&export=download </a></h4>';
+
+// Add CSS styling to the element
+element.style.backgroundColor = 'rgba(50, 50, 50, 0.5)';
+element.style.borderRadius = '10px';
+element.style.padding = '10px';
+element.style.margin = '10px';
+element.style.width = '75%';
+
+
+// Append the element to the 'stuff' div
+var container = document.getElementById('stuff');
+container.appendChild(element);
 
