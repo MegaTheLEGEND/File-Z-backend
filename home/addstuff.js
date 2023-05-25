ba97894048e059702290ba70018a0f394aa09fb1
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-05-25 10:45:17 -0700

    Update addstuff.js

--- home/addstuff.js
+++ home/addstuff.js
@@ -1,6 +1,6 @@
 // find the element
 var element = document.getElementById('stuff');
-element.innerHTML = '<h2> share with your friends! Share this download link.</h2> <br> <h4><a href="https://drive.google.com/uc?id=1xfplhlz0eWi-TgdGTpPcqIvqX-fVf508&export=download" >https://drive.google.com/uc?id=1xfplhlz0eWi-TgdGTpPcqIvqX-fVf508&export=download</a></h4>';
+element.innerHTML = '<h2> share with your friends! Share this download link.</h2> <br> <h4><a href="hhttps://drive.google.com/u/0/uc?id=1IZHSmcLa2UBGjNeO3GR_jaRPJO4oXz-u&export=download" >https://drive.google.com/u/0/uc?id=1IZHSmcLa2UBGjNeO3GR_jaRPJO4oXz-u&export=download</a></h4>';
 
 // Add CSS styling to the element
 element.style.backgroundColor = 'rgba(50, 50, 50, 0.5)';
