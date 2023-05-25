92bbbd148ffc32412e4b13c03eeccb5248a858c9
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-05-25 10:38:57 -0700

    Update about.js

--- about/about.js
+++ about/about.js
@@ -1,4 +1,4 @@
-var newestVersion = "2.1.2"; // this is the newest available version of the offline file system
+var newestVersion = "2.1.3"; // this is the newest available version of the offline file system
 
 
 function displayInfo() {
