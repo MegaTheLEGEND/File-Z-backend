911e41c92936cbf8ae69fbcb37255f834b21b509
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-05-31 11:03:14 -0700

    Update addstuff.js

--- home/addstuff.js
+++ home/addstuff.js
@@ -1,6 +1,6 @@
 // find the element
 var element = document.getElementById('stuff');
-element.innerHTML = '<h2> share with your friends! Share this download link.</h2> <br> <h4><a href="https://drive.google.com/u/0/uc?id=1IZHSmcLa2UBGjNeO3GR_jaRPJO4oXz-u&export=download" >https://drive.google.com/u/0/uc?id=1IZHSmcLa2UBGjNeO3GR_jaRPJO4oXz-u&export=download (FZ2.1.3.zip)</a></h4>';
+element.innerHTML = '<h2> share with your friends! Share this download link. download the newest version from this folder.</h2> <br> <h4><a href="https://drive.google.com/drive/folders/1XqZfzWx4XT3Rg8mHimjabHSg_VPg5sm5?usp=share_link" >https://drive.google.com/drive/folders/1XqZfzWx4XT3Rg8mHimjabHSg_VPg5sm5?usp=share_link (FZ2.1.3.zip)</a></h4>';
 
 // Add CSS styling to the element
 element.style.backgroundColor = 'rgba(50, 50, 50, 0.5)';
