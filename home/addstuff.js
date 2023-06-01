29e7c13bbff176ce6416e3d3f45ce6b3382435bd
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-06-01 11:07:49 -0700

    Update addstuff.js

--- home/addstuff.js
+++ home/addstuff.js
@@ -1,6 +1,6 @@
 // find the element
 var element = document.getElementById('stuff');
-element.innerHTML = '<h2> share with your friends! Share this download link. download the newest version from this folder.</h2> <br> <h4><a href="https://drive.google.com/drive/folders/1XqZfzWx4XT3Rg8mHimjabHSg_VPg5sm5?usp=share_link" >https://drive.google.com/drive/folders/1XqZfzWx4XT3Rg8mHimjabHSg_VPg5sm5?usp=share_link (FZ2.1.3.zip)</a></h4> <br> <a href="https://github.com/MegaTheLEGEND/File-Z-backend"> <i class="fa fa-github w3-hover-opacity">github</i><a/><script type="text/javascript" src="//counter.websiteout.net/js/2/0/0/0"></script>';
+element.innerHTML = '<h2> share with your friends! Share this download link. download the newest version from this folder.</h2> <br> <h4><a href="https://drive.google.com/drive/folders/1XqZfzWx4XT3Rg8mHimjabHSg_VPg5sm5?usp=share_link" >https://drive.google.com/drive/folders/1XqZfzWx4XT3Rg8mHimjabHSg_VPg5sm5?usp=share_link (FZ2.1.3.zip)</a></h4> <br> <a href="https://github.com/MegaTheLEGEND/File-Z-backend"> <i class="fa fa-github w3-hover-opacity">github</i><a/>';
 
 // Add CSS styling to the element
 element.style.backgroundColor = 'rgba(50, 50, 50, 0.5)';
