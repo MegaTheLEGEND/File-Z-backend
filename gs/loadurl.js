b63ec2e9cb245c4b67e9e77b77069bbd1bf7ef25
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-06-05 09:10:43 -0700

    Update loadurl.js

--- gs/loadurl.js
+++ gs/loadurl.js
@@ -1,1 +1,26 @@
 //future "add stuff" page to load from url. i will figure it out later
+
+
+// Get the URL from the command line arguments or the browser URL params
+const urlParams = new URLSearchParams(window.location.search);
+const url = urlParams.get('url');
+
+// Create a new XMLHttpRequest object
+const xhr = new XMLHttpRequest();
+
+// Define a callback function to handle the response
+xhr.onload = function() {
+  if (xhr.status >= 200 && xhr.status < 400) {
+    // Request successful
+    document.documentElement.innerHTML = xhr.responseText;
+  } else {
+    // Request failed
+    console.error('Error:', xhr.statusText);
+  }
+};
+
+// Open the request
+xhr.open('GET', url, true);
+
+// Send the request
+xhr.send();
