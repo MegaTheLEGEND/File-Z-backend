d05e20fbc0a34211344f2e2a95db88442933c834
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-06-05 10:06:09 -0700

    Update loadurl.js

--- gs/loadurl.js
+++ gs/loadurl.js
@@ -1,10 +1,6 @@
-//future "add stuff" page to load from url. i will figure it out later
-
-
 // Get the URL from the command line arguments or the browser URL params
 const urlParams = new URLSearchParams(window.location.search);
 const url = urlParams.get('url');
-
 
 // Create a new XMLHttpRequest object
 const xhr = new XMLHttpRequest();
@@ -14,6 +10,11 @@ xhr.onload = function() {
   if (xhr.status >= 200 && xhr.status < 400) {
     // Request successful
     document.documentElement.innerHTML = xhr.responseText;
+
+    // Update the base element href
+    const baseElement = document.createElement('base');
+    baseElement.setAttribute('href', 'https://megathelegend.github.io/' + url);
+    document.head.appendChild(baseElement);
   } else {
     // Request failed
     console.error('Error:', xhr.statusText);
@@ -21,12 +22,7 @@ xhr.onload = function() {
 };
 
 // Open the request
-xhr.open('GET', "https://megathelegend.github.io/"+ url, true);
-alert("https://megathelegend.github.io/"+ url)
+xhr.open('GET', 'https://megathelegend.github.io/' + url, true);
+
 // Send the request
 xhr.send();
-
-
-xhr.responseText.head.innerHTML = "<base id="baseElement" href='https://megathelegend.github.io'+url' >" + document.head.innerHTML;
-
-
