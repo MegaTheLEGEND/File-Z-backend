cbcf8311a2181643bf4be09a8f7be04ed267ec26
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-07-16 23:04:39 -0700

    Create neal.js

---
+++ gs/embedded/neal.js
@@ -0,0 +1,22 @@
+
+
+	const location = new URL(window.location.href);
+	const url = "https://neal.fun";
+
+	if (url.length === 0) {
+		alert("No URL specified");
+		return;
+	}
+
+	switch (location.searchParams.get("type")) {
+		default: {
+			const frame = document.createElement("embed");
+			frame.type = "text/plain";
+			frame.style.position = 'absolute';
+			frame.width = "100%";
+			frame.height = "100%";
+			frame.src = url;
+			document.body.appendChild(frame);
+			break;
+		}
+	}
