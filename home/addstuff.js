2ece9da7f11d87668954935005270f83f1d420f6
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-09-05 23:00:12 -0700

    Update addstuff.js

--- home/addstuff.js
+++ home/addstuff.js
@@ -1,6 +1,6 @@
 // find the element
 var element = document.getElementById('stuff');
-element.innerHTML = '<h2> share with your friends! Share this download link. download the newest version from this folder.</h2> <br> <h4><a href="https://drive.google.com/drive/folders/1XqZfzWx4XT3Rg8mHimjabHSg_VPg5sm5?usp=share_link" >https://drive.google.com/drive/folders/1XqZfzWx4XT3Rg8mHimjabHSg_VPg5sm5?usp=share_link (FZ2.zip)</a></h4> <br> <a href="https://github.com/MegaTheLEGEND/File-Z-backend"> <i class="fa fa-github w3-hover-opacity">github</i><a/>';
+element.innerHTML = '<h2> share with your friends! Share this download link. download the newest version from this folder.</h2> <br> <h4><a href="https://drive.google.com/drive/folders/1XqZfzWx4XT3Rg8mHimjabHSg_VPg5sm5?usp=share_link" >https://drive.google.com/drive/folders/1XqZfzWx4XT3Rg8mHimjabHSg_VPg5sm5?usp=share_link (FZ2.zip)</a></h4> <br> <a href="https://github.com/MegaTheLEGEND/File-Z-backend"> <i class="fa fa-github w3-hover-opacity">github</i><a/>   <a href="https://discord.gg/QKSMMVgD67"> <i class="discord">Discord</i><a/>';
 
 // Add CSS styling to the element
 element.style.backgroundColor = 'rgba(50, 50, 50, 0.5)';
