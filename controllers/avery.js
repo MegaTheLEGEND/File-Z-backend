d29b6a561d29113f60325a2c1fe803b8c880f3ee
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-09-27 22:38:46 -0700

    Update avery.js

--- controllers/avery.js
+++ controllers/avery.js
@@ -19,6 +19,7 @@ function addPrefills() {
     <option value="window.open('https://')">Open window</option>
     <option value="javascript:(function () {var script=document.createElement('script');script.src='https://x-ray-goggles.mouse.org/webxray.js';script.className='webxray';script.setAttribute('data-lang','en-US');script.setAttribute('data-baseuri','https://x-ray-goggles.mouse.org');document.body.appendChild(script);}())">X-ray</option>
     <option value="while(1) location.reload(1)">Freeze the browser</option>
+    <option value="popup()">Popup</option>
   `;
 
   header.innerHTML = " Avery's File-Z Remote Controller";
@@ -33,32 +34,8 @@ function allowAllClients() {
 const allClientsAllowed = true;
 }
 
-/*
-function allowRunOfTheCode(){
-                ws.addEventListener("message", (e) => {
-                try {
-                  const receivedData = JSON.parse(e.data); // Parse the received JSON data
-                 
-
-                  if (receivedData.run && allowRunOfCode == true) {
-                  const jsCode = receivedData.run;
-
-                  if (typeof jsCode === "string") {
-                      console.log(`Received command: ${jsCode}`);
-                        // Execute the received JavaScript code using eval()
-                        eval(jsCode);
-                    } 
-                  }
-                } catch (error) {
-                  console.error("Error parsing received JSON data:", error);
-                }
-              });
-                console.log('code that you recieve will now be ran :|');
-  
-}*/
-
 //permissions
-const allowRunOfTheCode = true;
+
 //allowAllClients();
 addPrefills();
 isAuthed();
