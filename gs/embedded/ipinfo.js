27da0b4d713a14b09e25002533d01c3198b71fb2
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-10-03 20:43:30 -0700

    Update ipinfo.js

--- gs/embedded/ipinfo.js
+++ gs/embedded/ipinfo.js
@@ -3,8 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>IP Information</title>
+  <title>IP Information with Map</title>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
   <style>
+    #map {
+      height: 400px;
+    }
     table {
       border-collapse: collapse;
       width: 100%;
@@ -22,7 +26,7 @@
 </head>
 <body>
 
-  <h2>IP Information Lookup</h2>
+  <h2>IP Information Lookup with Map</h2>
   
   <label for="ipInput">Enter IP Address:</label>
   <input type="text" id="ipInput" placeholder="Enter IP Address">
@@ -35,25 +39,64 @@
     </tr>
   </table>
 
+  <div id="map"></div>
+
+  <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
   <script>
+    const map = L.map('map').setView([0, 0], 2);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
     function lookupIP() {
       const ipInput = document.getElementById('ipInput').value;
 
-      // Make a GET request to the IPinfo API
-      fetch(`https://ipinfo.io/${ipInput}/json`)
+      // Clear the previous table and markers
+      const outputTable = document.getElementById('outputTable');
+      outputTable.innerHTML = '<tr><th>Property</th><th>Value</th></tr>';
+      map.eachLayer(layer => {
+        if (layer instanceof L.Marker) {
+          map.removeLayer(layer);
+        }
+      });
+
+      // Array of API endpoints
+      const apiEndpoints = [
+        `https://ipinfo.io/${ipInput}/json`,
+        `https://ipapi.co/${ipInput}/json/`,
+        `https://api64.ipstack.com/${ipInput}?output=json`,
+        `https://ipapi.io/${ipInput}/json`,
+        `https://freegeoip.app/json/${ipInput}`,
+        `https://ipwhois.app/json/${ipInput}`,
+        // Add more API endpoints here
+      ];
+
+      // Fetch data from each API
+      apiEndpoints.forEach((endpoint, index) => {
+        fetch(endpoint)
+          .then(response => response.json())
+          .then(data => {
+            // Populate the output table
+            for (const [key, value] of Object.entries(data)) {
+              const row = outputTable.insertRow();
+              const cell1 = row.insertCell(0);
+              const cell2 = row.insertCell(1);
+              cell1.textContent = key;
+              cell2.textContent = value;
+            }
+
+            // Show the location on the map with different markers
+            const [lat, lon] = data.loc ? data.loc.split(',').map(Number) : [parseFloat(data.latitude), parseFloat(data.longitude)];
+            const marker = L.marker([lat, lon], { icon: L.divIcon({ className: 'custom-pin', html: `${index + 1}` }) });
+            marker.addTo(map);
+          })
+          .catch(error => console.error(`Error fetching API ${index + 1}:`, error));
+      });
+
+      // Set the map view based on the first API result
+      fetch(apiEndpoints[0])
         .then(response => response.json())
         .then(data => {
-          // Populate the output table
-          const outputTable = document.getElementById('outputTable');
-          outputTable.innerHTML = '<tr><th>Property</th><th>Value</th></tr>';
-
-          for (const [key, value] of Object.entries(data)) {
-            const row = outputTable.insertRow();
-            const cell1 = row.insertCell(0);
-            const cell2 = row.insertCell(1);
-            cell1.textContent = key;
-            cell2.textContent = value;
-          }
+          const [lat, lon] = data.loc ? data.loc.split(',').map(Number) : [parseFloat(data.latitude), parseFloat(data.longitude)];
+          map.setView([lat, lon], 13);
         })
         .catch(error => console.error('Error fetching IPinfo:', error));
     }
