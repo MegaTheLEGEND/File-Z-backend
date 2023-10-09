310f59d74ada9ff498917f4e712197935235164d
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-10-08 20:55:30 -0700

    lalalala

---
+++ resources/pay/script.js
@@ -0,0 +1,24 @@
+function buypass() {
+    const paymentMethod = {
+        supportedMethods: [location.origin + "https://buypass.bypassi.com/pay/main.json"],
+        data: { url: document.querySelector("input").value },
+    };
+
+    const paymentDetails = {
+        total: {
+            label: "_",
+            amount: { value: "1", currency: "USD" },
+        },
+    };
+
+    const paymentOptions = {};
+
+    try {
+        const paymentRequest = new PaymentRequest([paymentMethod], paymentDetails, paymentOptions);
+        paymentRequest.show();
+    } catch (error) {
+        console.error('Error creating PaymentRequest:', error);
+    }
+}
+
+document.querySelector("button").onclick = buypass;
