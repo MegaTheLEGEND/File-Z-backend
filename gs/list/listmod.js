a332016a2f7615296a47412f65ae325517ad85fa
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2024-02-03 11:22:01 -0800

    Update listmod.js

--- gs/list/listmod.js
+++ gs/list/listmod.js
@@ -7,3 +7,25 @@ xhttp.onreadystatechange = function() {
 };
 xhttp.open("GET", "https://megathelegend.github.io/File-Z-backend/gs/list/list.html", true);
 xhttp.send();
+
+
+
+
+//handler for the search box
+
+    document.getElementById('gsearchbar').addEventListener('input', filterLinks);
+
+function filterLinks() {
+    console.log("Function triggered");
+
+    var searchValue = document.getElementById('gsearchbar').value.toUpperCase().replace(/ /g, '');
+    var links = document.querySelectorAll('#glist>a');
+
+    links.forEach(function(link) {
+        link.style.display = link.textContent.toUpperCase().includes(searchValue) ? 'block' : 'none';
+    });
+
+    if (searchValue.indexOf("RICK") !== -1) {
+        window.location.replace("css/other-assets/index.html");
+    }
+}
